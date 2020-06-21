a93fd89f95674fbe07ab3da1a775b8b5b3c91bed
Julio Cesar <53705234+reininy@users.noreply.github.com>
2020-06-21 20:35:35 -0300

    Add Formatter function

--- scripts/main.py
+++ scripts/main.py
@@ -2,20 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 from flask import request, jsonify
 
+
+def Formatter(string):
+    characters_to_remove = "!()@\n"
+    new_string = string
+    for character in characters_to_remove:
+        new_string = new_string.replace(character, "")
+
+    return new_string 
+
 def Gatry():
     page = requests.get('https://gatry.com/')
     soup = BeautifulSoup(page.text, 'html.parser') ## parsea o html colocando cada tag em uma linha
     ##divs_info = soup.find_all("div", class_='informacoes')
     
     list = []
+    id = 0
     for div in soup.find_all("article", class_='promocao'):
         dict = {} ## inicializa o dicionario
         
         name = div.find("h3")
+        formatted_name = Formatter(str(name.text))
         price = div.find("p", class_='preco').get_text()
         image = div.find("div", class_='imagem').find('img', src=True)
-        print(name)
-        dict['name'] = name.text
+        id = id + 1
+        dict['id'] = id
+        dict['name'] = formatted_name
         dict['price'] = price
         dict['image'] = image['src']
 
